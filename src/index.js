9c92145b8e7b2e8d98572cf5208eb967c3d74f5a
lvino <553269233@qq.com>
2019-01-25 00:10:42 +1100

    change the work structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React ,{ Component,Fragment }from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style.js';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import GlobalStyle from './style.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Index extends Component {
+    render() {
+      
+      return (
+          <Fragment>
+              <GlobalStyle />
+              <App />
+          </Fragment>
+          
+      );
+    }
+  }
+ReactDOM.render(<Index />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
