d740c643e9bebe2fb1b55394698cac26fe2342ed
lvino <553269233@qq.com>
2019-01-25 17:18:35 +1100

    header finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from "./common/header"
 
 class App extends Component {
   render() {
     
     return (
-      <h1>Hello world</h1>
+      <Header />
     );
   }
 }
