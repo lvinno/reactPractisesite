851bddb7118278dbf960d954dde34441b8370d3e
lvino <553269233@qq.com>
2019-01-29 19:33:24 +1100

    Updates

---
+++ precache-manifest.2c7677b4d93907ffc3a3a64408d147e2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "17808b8e3cffe758a144",
+    "url": "/reactPractisesite/static/js/runtime~main.17808b8e.js"
+  },
+  {
+    "revision": "2806b41ddb5cf33d8ccd",
+    "url": "/reactPractisesite/static/js/main.2806b41d.chunk.js"
+  },
+  {
+    "revision": "31221c2489e7c688129d",
+    "url": "/reactPractisesite/static/js/1.31221c24.chunk.js"
+  },
+  {
+    "revision": "38e66cc37860c6dc8ccb2f58578f28a5",
+    "url": "/reactPractisesite/index.html"
+  }
+];
